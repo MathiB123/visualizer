acce2ceddd61497e807bec729f346649ded0c17b
MathiB123 <mathi1@videotron.ca>
2025-04-06 11:19:36 -0400

    corrections

--- src/audio.py
+++ src/audio.py
@@ -3,19 +3,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-sample_rate, data = wavfile.read(r"src/music_sounds/allo.wav")
+# sample_rate, data = wavfile.read(r"src/music_sounds/cat.wav")
 
-# print(data.dtype) => format wav
-# wavfile.write(r"src/music_sounds/allo_reversed.wav", sample_rate, data[::-1, :])
+# # print(data.dtype) => format wav
+# # wavfile.write(r"src/music_sounds/.wav", sample_rate, data)
 
-duration = len(data)/sample_rate
-times = np.arange(0, duration, 1/sample_rate)
 
-# data[:, 0] => gauche, data[:, 1] => droite, c'est stéréo
-plt.rcParams["figure.figsize"] = (10, 5)
-plt.plot(times, data[:, 0], label="Gauche")
-plt.plot(times, data[:, 1], label="Droite")
-plt.xlabel("Time (s)")
-plt.ylabel("Amplitude")
-plt.legend()
-plt.show()
+
+# duration = len(data)/sample_rate
+# times = np.arange(0, duration, 1/sample_rate)
+
+# # data[:, 0] => gauche, data[:, 1] => droite, c'est stéréo
+
+# plt.rcParams["figure.figsize"] = (10, 5)
+# plt.plot(times, data[:, 0], label="Gauche")
+# plt.plot(times, data[:, 1], label="Droite")
+# plt.xlabel("Time (s)")
+# plt.ylabel("Amplitude")
+# plt.legend()
+# plt.show()
+
+# times = np.arange(0, 3, 1/48000)
+# data_1 = np.cos(2*np.pi*440*times, dtype=np.float32)
+
+# wavfile.write(r"src/music_sounds/aaa.wav", 48000, data_1)
+
+# plt.plot(times, data)
+# plt.show()
+
